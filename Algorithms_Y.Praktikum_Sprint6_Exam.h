809bca98feec2a8bcb3c58f722ea938a1420c1e0
Эдгар Рупшис <e.rupshis@gmail.com>
2021-10-25 22:10:30 +0200

    fixed S6 Exams Problems. Added comments

--- Algorithms_Y.Praktikum_Sprint6_Exam.h
+++ Algorithms_Y.Praktikum_Sprint6_Exam.h
@@ -11,62 +11,111 @@
 namespace s6_exam_problems {
 	using namespace std::literals;
 
-	//SEND ID: ???
-	struct Edge {
-		int from = -1;
-		int to = -1;
-		int weight = -1;
+	//SEND ID: 54903905	
 
-		bool operator < (const Edge& other) const {
-			return weight < other.weight;
-		}
-	};
-
-	struct EdgeHasher {
-		size_t operator() (const Edge& edge) const {
-			return int_hash(edge.from) + 37 * int_hash(edge.to) + 37 * 37 * int_hash(edge.to);
-		}
-		std::hash<int> int_hash;
-	};
-
-
-
-	class Graph {
+	//A_Graph - class that represents a graph. It has methods that finds SpanningTree of graph.
+	//Graph should be not oriented and with one adjacency component, otherwise it's not possible to create 
+	//spanning tree (return of method will be nullopt)
+	//Time complexity:
+	//   - constructor() - O(V) where's V is a count of verticies;
+	//   - AddEdge() - O(1) - add only one oriented / not oriented vertex in graph
+	//   - GetMaxSpinningTree() - O(E * logV), where's V - count of vertices, E - count of edges (actually 2*E).
+	//   This method check of vertecies in cycle and find biggest edge and add end-vertex of this edge in span. tree.
+	//   This operation repeats while all vertecies will not be added in spanning tree. If some vertices where not added 
+	//   in span. tree, than this graph has more than one adjacency component.			
+	//	 Search of biggest outcome edges - O(1) - thanks to priority queue.
+	//	 Adding of vertex's outcome edges - O(E * LogV), where's V - count of vertecies. This method go through all edges that 
+	//	 outcome form considering vertex O(E) and add adjacency vertex only it was not added in span tree before (LogV - complexity of priority_queue's 'push') 
+	//   
+	//Space complexity:
+	//   - Graph - O(V) + O(2*E). There's no need to keep adjacency info in matrix because  2E less than V^2
+	//   - AddEdge() - O(1) of additional space
+	//   - AddVertexInSpanningTree() - O(1) of additional space
+	//   - GetMaxSpanningTree() - O(V) + O(N) + O(E) where's V - count of vertices (added + not_added unord_sets), 
+	//   N - count of vertices in result span. tree,  E - count of considering edges in priority_queue.
+	
+	class A_Graph {
 	public:
-		explicit Graph(int n, bool oriented = false)
+		struct Edge {
+			int from = -1;
+			int to = -1;
+			int weight = -1;
+
+			bool operator < (const Edge& other) const {
+				return weight < other.weight;
+			}
+		};
+
+		struct EdgeHasher {
+			size_t operator() (const Edge& edge) const {
+				return int_hash(edge.from) + 37 * int_hash(edge.to) + 37 * 37 * int_hash(edge.to);
+			}
+			std::hash<int> int_hash;
+		};
+	public:
+		explicit A_Graph(int n, bool oriented = false)
 			:edges_(n + 1), oriented_(oriented) 
 		{
-			while (n > 0) {
-				vertices_.insert(n);
+			while (n > 0) { // graph is not oriented by default for task
+				vertices_.insert(n); // some vertices may be not be in adjacency components (vertecies without incident edges)
 				--n;
 			}
 		}
 
 		void AddEdge(int from, int to, int weight) {
-			if (from != to //zero edge from == to
-				&& (edges_[from].count(to) == 0 || edges_[from].at(to) < weight)) { // add new edge if edge is not added or rewrite old if new is bigger
+			if (from != to && (edges_[from].count(to) == 0 //zero edge from == to
+				|| edges_[from].at(to) < weight)) { // add new edge if edge is not added or rewrite old if new is bigger
 				edges_[from][to] = weight;
 				if (!oriented_) {
-					edges_[to][from] = weight; //not  oriented - we need to add opposite direction graph for easier search
+					edges_[to][from] = weight; //not oriented - we need to add opposite direction graph for easier search
 				}
 			}			
-		}
+		}		
 
-		std::unordered_set<int> GetVertices() const {
-			return vertices_;
-		}
+		std::optional<std::vector<Edge>> GetMaxSpanningTree() const {
+			std::vector<Edge> res;
+			std::unordered_set<int> added; // already added in span
+			std::unordered_set<int> not_added(vertices_); // should be added
+			std::priority_queue<Edge> edges; // edges that go out from span
 
-		const std::unordered_map<int, int>& GetEdgesFromVert(int v) const {
-			return  edges_[v];
+			int v = *not_added.begin();
+			AddVertexInSpanningTree(added, not_added, edges, v);
+
+			while (not_added.size() > 0 && edges.size() > 0) {
+				Edge e = edges.top(); // extract max weighted edge
+				edges.pop();
+
+				if (not_added.count(e.to)) { //if end vertex of this edges is not added in spaning tree
+					res.push_back(e); //add this edge
+					AddVertexInSpanningTree(added, not_added, edges, e.to); // add end vertex and outcome edges
+				}
+			}
+
+			if (not_added.size() > 0) { // no edges to some vertices
+				return std::nullopt;
+			}
+			else {
+				return res;
+			}
 		}
 
 	private:		
 		bool oriented_ = false;
 		std::unordered_set<int> vertices_{};
 		std::vector<std::unordered_map<int, int>> edges_;
+
+		void AddVertexInSpanningTree(std::unordered_set<int>& added, std::unordered_set<int>& not_added, std::priority_queue<Edge>& edges, int v) const {
+			added.insert(not_added.extract(v)); // move vertex		
+
+			for (auto& edge : edges_[v]) { // add edges that outcome form v
+				if (not_added.count(edge.first)) { // only if end vertex of edge was not already added in spanning tree
+					edges.push({ v, edge.first, edge.second });
+				}
+			}
+		}		
 	};
 
-	void AddEdgesInGraph(std::istream& input, Graph& graph) {
+	void AddEdgesInGraph(std::istream& input, A_Graph& graph) {
 		int m; // m - count of edges
 		input >> m;
 
@@ -75,53 +124,16 @@ namespace s6_exam_problems {
 			input >> f >> t >> w;
 			graph.AddEdge(f, t, w);
 		}
-	}
-
-	void AddVertexInSpanningTree(Graph& graph, std::unordered_set<int>& added, std::unordered_set<int>& not_added, std::priority_queue<Edge>& edges, int v) {
-		added.insert(not_added.extract(v)); // move 		
-
-		for (auto& edge : graph.GetEdgesFromVert(v)) { // add edges that outcome form v
-			if (not_added.count(edge.first)) { // only if end vertex of edge was not already added in spanning tree
-				edges.push({ v, edge.first, edge.second });
-			}
-		}
-	}
-
-	std::optional<std::vector<Edge>> FindMaxSpanningTree(Graph& graph) {
-		std::vector<Edge> res;
-		std::unordered_set<int> added; // already added in span
-		std::unordered_set<int> not_added(graph.GetVertices()); // should be added
-		std::priority_queue<Edge> edges; // edges that go out from span
-				
-		int v = *not_added.begin();
-		AddVertexInSpanningTree(graph, added, not_added, edges, v);
-
-		while (not_added.size() > 0 && edges.size() > 0) {
-			Edge e = edges.top(); // extract max weighted edge
-			edges.pop();
-
-			if (not_added.count(e.to)) { //if end vertex of this edges is not added in spaning tree
-				res.push_back(e); //add this edge
-				AddVertexInSpanningTree(graph, added, not_added, edges, e.to); // add end vertex and outcome edges
-			}
-		}
-
-		if (not_added.size() > 0) { // no edges to some vertices
-			return std::nullopt;
-		}
-		else {
-			return res;
-		}
-	}	
+	}		
 
 	void A_ExpensiveNetwork(std::istream& input, std::ostream& output) {
 		int n;
 		input >> n;
-		Graph graph(n);
+		A_Graph graph(n);
 
 		AddEdgesInGraph(input, graph);	
 
-		std::optional<std::vector<Edge>> max_spanning_tree(FindMaxSpanningTree(graph));
+		std::optional<std::vector<A_Graph::Edge>> max_spanning_tree(graph.GetMaxSpanningTree());
 
 		if (max_spanning_tree) {
 			int spanning_tree_length = 0;
@@ -134,38 +146,91 @@ namespace s6_exam_problems {
 			output << "Oops! I did it again\n"s;
 		}
 	}
-	/*-------------------------------------------------------------------------*/		
+	/*-------------------------------------------------------------------------*/	
+
+	//SEND ID: 55420607
+
+	//Class B_Graph - class that represents graph. Interface allows to initialize graph by list
+	//of edges and check cycles inside. Edges may be only oriented because checking of cycles in not oriented graph is useless.
+	//According task Graph has to find duplicates of routes between two cities (B roads and R roads). In this way, one of road type can be 
+	//considered as an opposite direction edges. Thanks to this, we just can check cycles in graph. Cycle will point on route dublicating 
+	//Storage of graph is performed as an adjacency lists. There's no need to keep edges as an adjacency matrix because
+	// every vertex has pow of outputs ~0.5*N (N - count of vertecies) on average.
+	// Time complexity: 
+	//   - constructor() - O(1) 
+	//   - AddEdge() - O(1) only one edge will be added at once. Therefore adding of all edges will take O(M), M - count of all edges
+	//   - IsAcyclic() - method that check cycles in graph. complexity is O(V + E), where's V - count of vertices and E - count of edges in graph
+	//   This methods checks all vertecies and if color of vertex is white (w) than it should be checked and all adjacency vertices (equal to pow of output of vertex) of 
+	//   this vertex should added in queue for checking (if it hadn't been checked before).
+	//
+	// Space complexity: 
+	//   - Graph: O(E) - where's E - count of edges. 
+	//   - constructor() - O(1) of additional memory
+	//   - AddEdge() - O(1) of additional memory
+	//   - DFS_Cycles() - O(V) of additional memory (V - count of verticies). It's used for storage of aux. information - highlighting of verticies and 
+	//   for stack of verticies to check
+	
+
 	class B_Graph {
-	public:
+	public:		
+		enum class EdgeType {
+			B,
+			R
+		};
 		explicit B_Graph(int n)
 			:matrix_(n + 1, std::vector<int>(n + 1, 0))
-		{
-			while (n > 0) {
-				vertices_.insert(n);
-				--n;
-			}
+		{			
 		}
 
-		void AddEdge(int from, int to, char type) {			
-			if (type == 'B') {
+		void AddEdge(int from, int to, EdgeType type) {			
+			if (type == EdgeType::B) { // front direction edge
 				matrix_[from][to] = 1;				
 			}
-			else {
-				matrix_[to][from] = 1;
+			else { // opposite direction edge
+				matrix_[to][from] = 1; 
 			}
 		}
 
-		const std::unordered_set<int>& GetVertices() const {
-			return vertices_;
+		bool IsAcyclic() { // check cycles in graph
+			return DFS_Cycles();
 		}
 
-		const std::vector<int>& GetEdgesFromVert(int v) const {
-			return matrix_[v];
-		}
-
-	private:		
-		std::unordered_set<int> vertices_{};
+	private:				
 		std::vector<std::vector<int>> matrix_;		
+
+		bool DFS_Cycles() {
+			int n = matrix_.size() - 1; // num of vertecies
+			std::vector<char> color(n + 1, 'w'); // mark all vertecies as white - not visited
+			std::stack<int> stack;
+
+			for (int i = 1; i < n; ++i) { // we have to check all vertices because  all verticies may not be reachable by B edges from first edge.
+				if (color[i] == 'w') {
+					stack.push(i); // stack of verticies to check
+					while (stack.size() > 0) {
+						int v = stack.top();
+						stack.pop();
+
+						if (color[v] == 'w') { // check if vertex was not handled before
+							color[v] = 'g';
+							stack.push(v);
+							
+							for (int j = 1; j < matrix_[v].size(); ++j) { // check all edges that incident to 'v' vertex
+								if (matrix_[v][j] && color[j] == 'w') { // add adjacency vertex for checking
+									stack.push(j);
+								}
+								else if (matrix_[v][j] && color[j] == 'g') { // cycle is found. No need to check farther
+									return false;
+								}
+							}
+						}
+						else { // move out handled vertex. It has gray (g) color
+							color[v] = 'b';
+						}
+					}
+				}
+			}
+			return true;
+		}
 	};
 
 	void FillGraph(B_Graph& graph, std::istream& input, int n) {
@@ -174,47 +239,17 @@ namespace s6_exam_problems {
 			std::string tmp;
 			std::getline(input, tmp);
 			for (int i = 0; i < tmp.size(); ++i) {
-				graph.AddEdge(j, j + 1 + i, tmp[i]);
+				if (tmp[i] == 'B') {
+					graph.AddEdge(j, j + 1 + i, B_Graph::EdgeType::B);
+				}
+				else {
+					graph.AddEdge(j, j + 1 + i, B_Graph::EdgeType::R);
+				}
 			}
 			++j;
 		}
 	}
-
-	bool DFS(B_Graph& graph, int idx) {
-		int n = graph.GetVertices().size();
-		std::vector<char> color(n + 1, 'w');
-		std::stack<int> stack;
-
-		for (int i = 1; i < n; ++i) {
-			if (color[i] == 'w') {
-				stack.push(i);
-				while (stack.size() > 0) {
-					int v = stack.top();
-					stack.pop();
-
-					if (color[v] == 'w') {
-						color[v] = 'g';
-						stack.push(v);
-
-						std::vector<int> tmp = graph.GetEdgesFromVert(v);
-						for (int j = 1; j < tmp.size(); ++j) {
-							if (tmp[j] && color[j] == 'w') {
-								stack.push(j);
-							}
-							else if (tmp[j] && color[j] == 'g') {
-								return false;
-							}
-						}
-					}
-					else {
-						color[v] = 'b';
-					}
-				}
-			}
-		}
-		return true;
-	}
-
+	
 	void B_RailRoads(std::istream& input, std::ostream& output) {
 		int n;
 		input >> n;
@@ -222,16 +257,14 @@ namespace s6_exam_problems {
 		B_Graph graph(n);
 		FillGraph(graph, input, n);		
 
-		if (DFS(graph, 1)) {
+		if (graph.IsAcyclic()) {
 			output << "YES"s << '\n';
 		}
 		else {
 			output << "NO"s << '\n';
-		}
-		
+		}		
 	}
 }
-
 
 namespace s6_exam_tests {
 	using namespace std::literals;
